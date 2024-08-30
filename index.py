f1b13d5635c4593fa272f5cfa7825b0f3e3c4073
Harrycarter555 <top9pdf@gmail.com>
2024-08-30 20:53:58 +0530

    Update index.py

--- index.py
+++ index.py
@@ -144,7 +144,7 @@ def respond():
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    webhook_url = f'https://your-app-url/{TOKEN}'  # Update with your deployment URL
+    webhook_url = f'https://harrycarter555.vercel.app/{TOKEN}'  # Update with your deployment URL
     s = bot.setWebhook(webhook_url)
     if s:
         return "Webhook setup ok"
